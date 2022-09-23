4e89f77d569a379cb7fabbf8c157ad1ef7b64973
Samuelknis <90838614+Nordic65@users.noreply.github.com>
2022-09-23 18:26:35 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>email = test@test.com; password = 123</p>
+    <form method="post" action="data.php">
+        <lable for="email">Enter thy electronic mail</lable>
+        <br>
+        <input type="text" name="email">
+        <br>
+        <lable for="password">Enter thy password for thy name</lable>
+        <br>
+        <input type="text" name="password">
+        <br>
+        <input type="submit" value="Press me you scallywag">
+    </form>
+</body>
+
+</html>
